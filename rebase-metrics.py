defd7695fa5625671b7be4768c0eeeb4c51b0e79
Ben Clapp <clappben@gmail.com>
2018-05-27 18:37:46 +1200

    refactor, add args for server, time and output

--- rebase-metrics.py
+++ rebase-metrics.py
@@ -3,11 +3,21 @@
 import requests
 import time
 import yaml
+import argparse
 
-prom_server = "<promethues_server_goes_here>"
-query_url=prom_server + '/api/v1/query'
-rebase_time = int(time.time())
-rule_file_name = "data.yml"
+parser = argparse.ArgumentParser(description='Converts current prometheus metrics, to recording rules at a point in time.')
+parser.add_argument('-s', '--server', type=str, dest='server', required=True,
+                    help='The prometheus server to query. Include protocol, fqdn and port')
+parser.add_argument('-t', '--time', type=int, dest='custom_time', default=int(time.time()),
+                    help='A custom time to query at, as UNIX time in seconds. Defaults to now.')
+parser.add_argument('-o', '--output-file', type=str, dest='output_file', default='data.yaml',
+                    help='Where to save the rule files. Default to "data.yaml"')
+
+args = parser.parse_args()
+
+query_url = args.server + '/api/v1/query'
+rebase_time = args.custom_time
+rule_file_name = args.output_file
 
 metric_io_table = [
     dict(
@@ -31,13 +41,16 @@ metric_io_table = [
         group_name = "rebased gtp_completed_transactions at " + str(rebase_time)
     ),
     dict(
-        query = str('increase(gtp_completed_loyalty_transactions{OrganisationName!="Availability Test Vendor"}[1y])'),
+        query = str('round(increase(gtp_completed_loyalty_transactions{OrganisationName!="Availability Test Vendor"}[1y]))'),
         metric_to_record = 'gtp_completed_loyalty_transactions:rebase',
         group_name = "rebased gtp_completed_loyalty_transactions at " + str(rebase_time)
     )
 ]
 
-def query_metrics(query: str):
+def pause():
+    scriptPause = input("Press <ENTER> to continue.")
+
+def query_prometheus(query: str):
     """Used to query the configured prometheus instance for it's metrics.
 
     Args:
@@ -46,8 +59,8 @@ def query_metrics(query: str):
     Returns:
         list: A list of metrics, essentially the result list from the Prometheus HTTP API.
     """
-    query_params = {'query': str(query), 'time': rebase_time}
-    print("query_params:" + str(query_params))
+    print("Querying " + query)
+    query_params = {'query': query, 'time': rebase_time}
     r = requests.get(query_url, params=query_params)
     r.raise_for_status()
     response = r.json()
@@ -65,8 +78,8 @@ def convert_metrics_to_rules(metrics: list, output_metric: str):
     Returns:
         rules: a list of rules to be placed in a recording rule group
     """
+    print("Creating rules for " + output_metric)
     rules = []
-    print("Converting metrics to rules for " + output_metric)
     for metric in metrics:
         # print(metric)
         rule = dict(
@@ -83,13 +96,21 @@ def convert_metrics_to_rules(metrics: list, output_metric: str):
         rules.append(rule)
     return rules
     
+print('About to convert the result of these queries:')
+for metric in metric_io_table:
+    print(metric["query"])
+
+print('with these arguments:')
+print(str(args))
+pause()
+
 rule_file = dict(
     groups = []
 )
 
 for metric in metric_io_table:
     print('Generating rules for group: "' + metric["group_name"] + '"')
-    metrics = query_metrics(metric["query"])
+    metrics = query_prometheus(metric["query"])
     rules = convert_metrics_to_rules(metrics, metric["metric_to_record"])
 
     group = dict(
@@ -100,6 +121,7 @@ for metric in metric_io_table:
 
     rule_file["groups"].append(group)
     
+
 print("Writing to " + rule_file_name)
 with open(rule_file_name, 'w') as outfile:
     yaml.dump(rule_file, outfile, default_flow_style=False)
